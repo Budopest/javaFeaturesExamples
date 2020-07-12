79829fed213f66a73e9548884bcebae16100de87
budopest <abdo.mohamed5111995@gmail.com>
2020-07-12 23:27:09 +0200

    Geniric functional interfaces implementation using block lambda

--- src/main/java/lambda/SimpleFunctionalInterface.java
+++ src/main/java/lambda/SimpleFunctionalInterface.java
@@ -1,7 +1,32 @@
 package lambda;
 
 public class SimpleFunctionalInterface {
+    public static void main(String[] args) {
+        GeniricI<Integer> factorial = (num) -> {
+            int result = 1;
+            for (int i = 2; i <= num; i++) {
+                result *= i;
+            }
+            return result;
+        };
+        System.out.println(factorial.specialFunc(5));
+
+        GeniricI<String> reverse = (word) -> {
+            String result = "";
+            for(int i=word.length()-1;i>=0;i--){
+                result += word.charAt(i);
+            }
+            return result;
+        };
+        System.out.println(reverse.specialFunc("oops"));
+
+    }
 }
 interface NumberTester{
     boolean test(int number);
+}
+interface GeniricI<T>{
+    //T specifies both the return type and the parameter type of func( ).
+    // This means that it is compatible with any lambda expression that takes one parameter and returns a value of the same type.
+    T specialFunc(T obj);
 }
--- src/test/java/lambda/LambdaSpecs.java
+++ src/test/java/lambda/LambdaSpecs.java
@@ -7,14 +7,17 @@ public class LambdaSpecs {
 
     @Test
     void numberTwoIsEven(){
-        NumberTester numberTester;
-        numberTester = (num) -> num%2 ==0;
-        assertTrue(numberTester.test(2),"Number two should be detected as even number");
+        NumberTester isEven;
+        //type of n is not specified. Rather, its type is inferred from the context.
+        // In this case, its type is inferred from the parameter type of test()
+        isEven = (num) -> num%2 ==0;
+        assertTrue(isEven.test(2),"Number two should be detected as even number");
     }
     @Test
     void numberIsNegative(){
-        NumberTester numberTester;
-        numberTester = (num) -> num<0;
-        assertTrue(numberTester.test(-3),"Number -3 should be found negative");
+        NumberTester isNegative;
+        //It is also possible to explicitly specify the type of a parameter in a lambda expression.
+        isNegative = (int num) -> num<0;
+        assertTrue(isNegative.test(-3),"Number -3 should be found negative");
     }
 }
